2cf5749fb29b621fb9a6e6c61a5a5b4b9511d77b
ercanbrgr0 <brgrercan@gmail.com>
2026-02-22 01:43:22 +0300

    sw cache tamamen kaldirildi hata duzeltildi

--- sw.js
+++ sw.js
@@ -1,20 +1,11 @@
-const CACHE = 'ercan-brgr-v9';
-const FILES = ['/envanter-site/', '/envanter-site/index.html', '/envanter-site/icon.svg'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
+// Service Worker devre disi
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });

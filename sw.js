042540f43622fc41e016c350cc3157e19c2e2876
ercanbrgr0 <brgrercan@gmail.com>
2026-02-22 03:43:17 +0300

    force cache clear sw fix

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-// Service Worker devre disi
+// Service Worker - tamamen devre disi, kendini siliyor
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.matchAll({includeUncontrolled:true}))
+    .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(fetch(e.request, {cache:'no-store'}));
 });

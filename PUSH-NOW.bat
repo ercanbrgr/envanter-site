ccbf6fd91486410ad00854a1f35523a48c741e6a
ercanbrgr <brgrercan@gmail.com>
2026-03-09 11:43:27 +0300

    Update PUSH-NOW.bat

--- PUSH-NOW.bat
+++ PUSH-NOW.bat
@@ -2,12 +2,9 @@
 cd /d "%~dp0"
 set PATH=%PATH%;C:\Program Files\Verdent\resources\app.asar.unpacked\node_modules\dugite\git\cmd
 
-echo === GITHUB'A YUKLENIYOR ===
-git add .
-git commit -m "Recete yonetimi eklendi"
-git push origin main --force-with-lease
+echo === PUSH YAPILIYOR ===
+git push origin main --force
 
 echo.
 echo === BITTI ===
-echo 5 dakika bekle, sonra siteyi yenile!
 pause

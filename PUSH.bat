5f0acc553970c390f4d2f9d4a76ee58f4133cb95
ercanbrgr <brgrercan@gmail.com>
2026-03-30 01:47:35 +0300

    Fix: Tenders ayri urun

--- PUSH.bat
+++ PUSH.bat
@@ -1,15 +1,11 @@
 @echo off
-set SRC=C:\Users\yapic\OneDrive\Belgeler\verdent-projects\new-project\envanter-site
-set DST=C:\Users\yapic\OneDrive\Belgeler\GitHub
+set REPO=C:\Users\yapic\OneDrive\Belgeler\verdent-projects\new-project\envanter-site
 set GIT=C:\Users\yapic\AppData\Local\GitHubDesktop\app-3.5.5\resources\app\git\cmd\git.exe
 
-echo Dosyalar kopyalaniyor...
-copy /Y "%SRC%\index.html" "%DST%\index.html"
-
 echo Git push yapiliyor...
-"%GIT%" -C "%DST%" add index.html
-"%GIT%" -C "%DST%" commit -m "guncelleme"
-"%GIT%" -C "%DST%" push
+"%GIT%" -C "%REPO%" add .
+"%GIT%" -C "%REPO%" commit -m "Fix: Tenders ayri urun"
+"%GIT%" -C "%REPO%" push
 
 echo Tamamlandi!
 pause
